1254f68390a6c5fadd22a0d7620b3f02d400b474
HanSeong Lee <jkhanseong@gmail.com>
2022-03-23 20:15:52 +0900

    ADD | Link on country container

--- containers/CountryContainer/index.tsx
+++ containers/CountryContainer/index.tsx
@@ -1,6 +1,7 @@
 import React, {HTMLAttributes, useEffect} from "react";
 import CountryCard from "../../components/CountryCard";
 import useCountries from "../../lib/useCountries";
+import Link from 'next/link';
 
 const CountryContainer: React.FC<HTMLAttributes<HTMLDivElement>> = (props) => {
     const { allCountries, countries } = useCountries();
@@ -12,13 +13,19 @@ const CountryContainer: React.FC<HTMLAttributes<HTMLDivElement>> = (props) => {
     return (
         <div {...props}>
             {countries?.map(({flags: {png}, name, region, capital, population}, index) => (
-                <CountryCard name={name}
-                             region={region}
-                             capital={capital}
-                             population={population}
-                             flag={png}
-                             key={index}
-                />
+                <Link href={`/countries/${String(name).toLowerCase()}`}
+                      key={index}
+                >
+                    <a>
+                        <CountryCard name={name}
+                                     region={region}
+                                     capital={capital}
+                                     population={population}
+                                     flag={png}
+
+                        />
+                    </a>
+                </Link>
             ))}
         </div>
     );
