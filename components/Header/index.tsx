4cf2939048022fe95d237244549dd72fffe4a329
HanSeong Lee <jkhanseong@gmail.com>
2022-03-24 21:30:52 +0900

    ADD | Link to title

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,16 +2,21 @@ import React from "react";
 import styles from './style.module.scss';
 import Container from "../Container";
 import DarkModeButton from "../../containers/DarkModeButton";
+import Link from 'next/link';
 
 const Header: React.FC = () => {
     return (
         <header className={styles.header}>
             <Container className={styles.container}>
-                <h1 className={styles.title}>
-                    Where in the world?
-                </h1>
+                <Link href={'/'}>
+                    <a>
+                        <h1 className={styles.title}>
+                            Where in the world?
+                        </h1>
+                    </a>
+                </Link>
 
-                <DarkModeButton className={styles.darkModeButton} />
+                <DarkModeButton className={styles.darkModeButton}/>
             </Container>
         </header>
     );
