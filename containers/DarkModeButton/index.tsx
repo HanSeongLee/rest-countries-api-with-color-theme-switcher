7038a190eaa2772238356ddfff026bb05dc38b01
HanSeong Lee <jkhanseong@gmail.com>
2022-04-03 21:54:58 +0900

    ADD | Resolved theme

--- containers/DarkModeButton/index.tsx
+++ containers/DarkModeButton/index.tsx
@@ -2,22 +2,27 @@ import React, {ButtonHTMLAttributes, useCallback, useEffect, useState} from "rea
 import {useTheme} from "next-themes";
 
 const DarkModeButton: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = (props) => {
-    const { theme, setTheme } = useTheme();
-    const [clientTheme, setClientTheme] = useState('light');
+    const { theme, setTheme, resolvedTheme } = useTheme();
+    let src;
 
     const onClick = useCallback(() => {
         setTheme(theme === 'dark' ? 'light' : 'dark');
     }, [theme]);
 
-    useEffect(() => {
-        setClientTheme(theme);
-    }, [theme]);
+    switch (resolvedTheme) {
+        case 'dark':
+            src = '/img/icon-crescent.svg';
+            break;
+        default:
+            src = '/img/icon-crescent-outline.svg';
+            break;
+    }
 
     return (
         <button onClick={onClick}
                 {...props}
         >
-            <img src={clientTheme === 'light' ? '/img/icon-crescent-outline.svg' : '/img/icon-crescent.svg'}
+            <img src={src}
                  alt={''}
             />
             Dark Mode
